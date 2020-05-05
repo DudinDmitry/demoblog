72340857d691926dc53a97a3163ba135edb66097
Dmitry <dimon.dudin1996@gmail.com>
2020-05-05 14:26:52 +0300

    Add cookie

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -24,5 +24,14 @@ Route::get('post/{id}','Post@showOne');
 Route::get('posts','Post@showAll');
 Route::get('/product/{category_id}/{product_id}','Product@showProduct');
 Route::get('/product/{category_id}','Product@showCategory');
+Route::get('/categories','Product@showCategoryList');
+Route::match(['get','post'],'/forms','TestController@formResult');
+Route::post('/forms/result','TestController@formResult');
+Route::get('/user/{id}/{age}','TestController@userId');
+Route::match(['get','post'],'/tests/method','TestController@testMethod');
+Route::get('/redirect','TestController@redirect');
+Route::get('/redirect/show/{param1}/{param2}','TestController@toRedirect')->name('show');
+Route::get('flash','TestController@flash');
+Route::get('flash/show','TestController@flashShow')->name('flashShowName');
 
 Route::get('test/sum/{num1}/{num2}','test@sum')->where(['num1'=>'[0-9]+','num2'=>'[0-9]+']);
