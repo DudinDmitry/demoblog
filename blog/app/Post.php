cb7f1a7cdb34ded80a075b118777395f71a9c3dd
Dmitry <dimon.dudin1996@gmail.com>
2020-05-06 14:21:05 +0300

    add delete

---
+++ blog/app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+`
+public $timestamps=false;
+
+}
